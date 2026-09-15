704dff2a7dc2fcec11330960e6ad626974bb8e26
The-G-Man-Half-Life <mateomoospina@gmail.com>
2026-09-15 06:57:48 -0500

    feat: finished all the process of the square root

--- sqrt.asm
+++ sqrt.asm
@@ -1,11 +1,11 @@
-// Valor del cual deseamos obtener la raiz cuadrada
-@16
+// Valor del cual deseamos obtener la raiz cuadrada en el espacio de ram 0
+@0
 D=A
 @0
 M=D
 
-// Inicializamos la respuesta en 0
-@Solution
+// Inicializamos la respuesta en 0 dentro del espacio de ram 1
+@1
 M=0
 
 // x es el número que se va a probar como posible raíz cuadrada
@@ -20,9 +20,9 @@ M=0
     @x
     D=M
     @a
-    M=D        // a = x (el número que sumaremos varias veces)
+    M=D        // a = x (el número que sumaremos varias veces, multiplicando)
     @b
-    M=D        // b = x (cuenta cuántas veces falta sumar)
+    M=D        // b = x (cuenta cuántas veces falta sumar, multiplicador)
 
 (MULTIPLICATION_LOOP)
     // si b == 0, terminamos de multiplicar
@@ -37,7 +37,7 @@ M=0
     @prod
     M=M+D
 
-    // reducimos en 1 la cantidad de veces que falta sumar
+    // reducimos en 1 la cantidad de veces que falta sumar al multiplicador
     @b
     M=M-1
 
@@ -47,7 +47,7 @@ M=0
     @prod
     D=M
     @R0
-    D=D-M          
+    D=D-M          //Realizamos la resta para verificar si nos pasamos o no de la respuest
 
     // si (prod - R0) > 0, nos pasamos: terminamos el proceso
     @END
@@ -56,14 +56,14 @@ M=0
     // si no nos pasamos, x sigue siendo una raíz válida: la guardamos
     @x
     D=M
-    @Solution
+    @1
     M=D
 
     // probamos con el siguiente número
     @x
     M=M+1
 
-    // volvemos al inicio del bucle
+    // volvemos al inicio del bucle para probar otro numero
     @LOOP
     0;JMP
 
